25e671b33af135015090e939e036f85a6a8c0457
merryn <h1335@naver.com>
2020-05-12 23:56:43 +0900

    first commit new repository

---
+++ TodoList/todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=144)
+    subtitle = models.CharField(max_length=144, blank = True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
